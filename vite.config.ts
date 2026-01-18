5b92104a53087c82fccff35fbdbeb672647d21dc
smithaap <smitha.ap@gmail.com>
2026-01-18 07:40:54 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/kape-bean-journey/",   // 👈 ADD THIS LINE
-
+  base: "/",
   server: {
     host: "::",
     port: 8080,
